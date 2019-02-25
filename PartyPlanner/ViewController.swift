5dfb95b94d6761447593e9e5a6fb36da44dd97a6
user150978 <user150978@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-02-25 17:16:52 -0500

    Initial Commit

---
+++ PartyPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyPlanner
+//
+//  Created by user150978 on 2/25/19.
+//  Copyright © 2019 Tion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
